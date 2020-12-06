02c3ed16284718134c0a77209df668aa285f69e8
imrannewgen <Imran@123$%^>
2020-12-06 12:04:22 +0600

    cache clear

--- src/main/java/com/chat/LMSChatApplication.java
+++ src/main/java/com/chat/LMSChatApplication.java
@@ -3,10 +3,17 @@ package com.chat;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class LMSChatApplication {
+public class LMSChatApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LMSChatApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LMSChatApplication.class, args);
@@ -16,6 +23,7 @@ public class LMSChatApplication {
 	CommandLineRunner init(){
 		return args -> System.out.println("<================>");
 	}
+
 
 
 }
